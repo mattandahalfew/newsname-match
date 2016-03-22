64127b0482ef49468bac285c4fa46c6dd8b8d5b7
mattandahalfew <mattandahalfew@gmail.com>
2016-03-22 17:28:46 -0400

    Steps towards Dedupe

--- newsname-match/newsname-match.py
+++ newsname-match/newsname-match.py
@@ -1,16 +1,19 @@
 #Title:         NewsName Matcher
-#Version:       1.0
-#Author:        Sasan Bahadaran
-#Date:          2/23/16
+#Orig. Author:  Sasan Bahadaran
+#Forked by:     Matt Anderson
+#Date:          3/15/16
 #Organization:  District Data Labs
 
 ##############################################
 #   IMPORTS
 ##############################################
 import os,sys
-import nltk, re, pprint
-import codecs
-from fuzzywuzzy import fuzz
+import nltk, re#, pprint
+#import codecs
+#from fuzzywuzzy import fuzz
+import dedupe
+import csv
+from unidecode import unidecode
 
 #map for name variations
 name_map = {}
@@ -25,31 +28,38 @@ class Dictlist(dict):
         except KeyError:
             super(Dictlist, self).__setitem__(key, [])
         self[key].append(value)
+        
+def preProcess(column):
+    try : # python 2/3 string differences
+        column = column.decode('utf8')
+    except AttributeError:
+        pass
+#    column = unidecode(column)
+    column = re.sub('  +', ' ', column)
+    column = re.sub('\n', ' ', column)
+    column = column.strip().strip('"').strip("'").lower().strip()
+#If data is missing, indicate that by setting the value to None
+    if not column:
+        column = None
+    return column
 
-def performNameExtraction(text):
-	#Returns a list of what NLTK defines as persons after processing the text passed into it.
-	try:
-		entity_names = []
-		for sent in nltk.sent_tokenize(text):
-			for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
-				if hasattr(chunk, 'label') and chunk.label:
-    					if chunk.label() == 'PERSON':
-						name_value = ' '.join(child[0] for child in chunk.leaves())
-						if name_value not in entity_names:
-							entity_names.append(name_value)
-	except:
-		print "Unexpected error:", sys.exc_info()[0]
-	return entity_names
+def performNameExtraction(text, p_entity_names):
+    #Returns a list of what NLTK defines as persons after processing the text passed into it.
+    try:
+        for sent in nltk.sent_tokenize(text):
+            for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+                if hasattr(chunk, 'label') and chunk.label:
+                    if chunk.label() == 'PERSON':
+                        name_value = ' '.join(child[0] for child in chunk.leaves())
+                        if name_value not in entity_names:
+                            p_entity_names.append(name_value)
+    except:
+        print('Unexpected error:' + sys.exc_info()[0])
+
+    return p_entity_names
 
 def processFiles():
-	#process every file in data folder
-	data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..', 'fixtures'))
-	pathjoin = os.path.join
-	for fn in os.listdir(data_dir):
-   		if not fn.startswith('otherfiles'):
-			with codecs.open(pathjoin(data_dir, fn), 'r',encoding='utf-8', errors='ignore') as text_file:
-        			text = text_file.read()
-        			entity_names = performNameExtraction(text)
+
 
 	return entity_names
 
@@ -58,33 +68,31 @@ def normalize_field(value):
 	
 	return value.lower().replace(" ", "")
 
-def fuzzblock(n1, n2, threshold):
-	"""
-	Returns True if the similarity of n1 and n2 is above the threshold.
-	"""
-
-	return similarity(n1, n2) > threshold
-
-def similarity(n1, n2):
-	"""
-	Returns the mean of the partial_ratio score for each field in the two
-	entities. Note that if they don't have fields that match, the score will
-	be zero.
-	"""
-
-	scores = [
-		fuzz.partial_ratio(n1, n2)
-    	]
-
-	return float(sum(s for s in scores)) / float(len(scores))
 
 if __name__ == '__main__':
-	entity_names = processFiles()
-	for e_name in entity_names:
-		for c_name in candidates:
-			if e_name not in name_map.values():
-				if fuzzblock(c_name,e_name,85):
-					name_map[c_name] = e_name
+    	#process every file in data folder
+    entity_names = []
+    data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..', 'fixtures'))
+#	pathjoin = os.path.join
+    for fn in os.listdir(data_dir):   		
+        if not fn.startswith('otherfiles'):
+            f = open(os.path.join(data_dir,fn))
+            reader = csv.DictReader(f)
+            text = ''
+            for row in reader:
+                clean_row = [(k, preProcess(v[0])) for (k, v) in row.items()]
+                l = len(clean_row)
+                for i in range(0,l):
+                    temp_row = clean_row[i]
+                    l_j = len(temp_row)
+                    for j in range(0,l_j):
+                        text = text + temp_row[j]
+            entity_names = []
+            entity_names = performNameExtraction(text, entity_names)
+#                entity_names = performNameExtraction(text)
 
-	for key,value in name_map.iteritems():
-		print 'key:' + key,'value: ' + value
+    fields = [
+    {'field' : 'First name', 'type': 'String', 'has missing' : True},
+    {'field' : 'Last name', 'type': 'String', 'has missing' : False},
+    ]
+    deduper = dedupe.Dedupe(fields)
